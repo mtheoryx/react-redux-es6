11aeff75e9fed9265a445df16292973ba233168a
David Poindexter <drpoindexter@gmail.com>
2017-06-11 14:19:39 -0400

    Add example of ES5 create class component

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
 /* eslint-disable no-console */
 console.log('hi');
+// Old ES5 way of creating a Class Component
+// var HelloWorld = React.createClass({
+//   render: function() {
+//     return (
+//       <h1>Hello World!</h1>
+//     );
+//   };
+// });
