618d0c602b32fb804b3eacae9a2e9770b1aa9273
David Poindexter <drpoindexter@gmail.com>
2017-06-18 13:14:04 -0400

    Add connected container test, and another form validation

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -5,7 +5,7 @@ import toastr from 'toastr';
 import * as courseActions from '../../actions/courseActions';
 import CourseForm from './CourseForm';
 
-class ManageCoursePage extends React.Component {
+export class ManageCoursePage extends React.Component {
   constructor(props, context) {
     super(props, context);
 
@@ -35,8 +35,26 @@ class ManageCoursePage extends React.Component {
     return this.setState({course: course});
   }
 
+  courseFormIsValid() {
+    let formIsValid = true;
+    let errors = {};
+
+    if (this.state.course.title.length < 5) {
+      errors.title = 'Title must be at least 5 characters.';
+      formIsValid = false;
+
+      this.setState({errors: errors});
+      return formIsValid;
+    }
+  }
+
   saveCourse(event) {
     event.preventDefault();
+
+    if (!this.courseFormIsValid()) {
+      return;
+    }
+
     this.setState({ saving: true });
     this.props.actions.saveCourse(this.state.course)
       .then(() => this.redirect())
